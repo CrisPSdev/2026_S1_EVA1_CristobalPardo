c1e8c1d01c15a34210b0a6780c63333c6943fbc7
CrisPSdev <c.pardos.sp@gmail.com>
2026-03-31 10:07:26 -0300

    primer commit

---
+++ src/main/java/cl/duoc/cristobalpardo/CristobalpardoApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.cristobalpardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristobalpardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristobalpardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/cristobalpardo/CristobalpardoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.cristobalpardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristobalpardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
