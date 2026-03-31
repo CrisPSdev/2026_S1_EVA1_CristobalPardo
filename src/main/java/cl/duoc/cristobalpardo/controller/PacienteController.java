0867c615aaac3a95166e55aa12daf11f03c97109
CrisPSdev <c.pardos.sp@gmail.com>
2026-03-31 11:58:46 -0300

    avance controller

--- src/main/java/cl/duoc/cristobalpardo/controller/PacienteController.java
+++ src/main/java/cl/duoc/cristobalpardo/controller/PacienteController.java
@@ -1,5 +1,30 @@
 package cl.duoc.cristobalpardo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.duoc.cristobalpardo.model.Paciente;
+import cl.duoc.cristobalpardo.service.PacienteService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
 public class PacienteController {
+
+    @Autowired 
+    PacienteService pacienteService;
+    
+    @GetMapping
+    public List<Paciente> listarPacientes(){
+        return pacienteService.retornarLista();
+    }
+    
+    
 
 }
