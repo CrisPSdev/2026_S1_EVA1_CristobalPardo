f216a9a9e7cb1e06b726c1e3e442db255656664f
CrisPSdev <c.pardos.sp@gmail.com>
2026-04-02 12:32:08 -0300

    service y controller arreglados y evidencia completa

--- src/main/java/cl/duoc/cristobalpardo/service/PacienteService.java
+++ src/main/java/cl/duoc/cristobalpardo/service/PacienteService.java
@@ -42,7 +42,7 @@ public class PacienteService {
             return "Solicitud eliminada";
         }
         else{
-            return "Solicitud no encontrada";
+            return null;
         }
     }
 
